13ef36fde0fc4fff44ff5079e8e4700dbc5a3885
Olena Kozachenko <olena.kozachenko.23@gmail.com>
2023-10-28 12:48:10 +0200

    Part 2 for JS

--- game.js
+++ game.js
@@ -1,9 +1,36 @@
 const gameField = document.querySelectorAll('.fiel');
-
-console.log(gameField);
-
 const gameBoard = document.querySelector('#game-board');
 
-gameBoard.addEventListener('click', () => (
-    console.log('click!')
-));
+const gameState = [
+    [gameField[0], gameField[1],gameField[2]],
+    [gameField[3], gameField[4],gameField[5]],
+    [gameField[6], gameField[7],gameField[8]],
+]
+gameBoard.addEventListener('click', (event) => {
+    const target = event.target;
+
+    let x, y;
+
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            if (column === target) {
+                x = rowIndex;
+                y = columnIndex;
+            }
+        });
+    });
+
+    let emptyX, emptyY;
+
+    gameState.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            if (column.innerText === '') {
+                emptyX = rowIndex;
+                emptyY = columnIndex;
+            }
+        });
+    });
+
+    console.log(x, y);
+    console.log(emptyX, emptyY);
+});
